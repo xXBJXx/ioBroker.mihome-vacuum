666f50325d6631c258a2e62710bd5ecf7bcba83b
Issi <issi.dev.iobroker@gmail.com>
2026-07-26 15:59:50 +0200

    feat: add Xiaomi QR code cloud login

--- main.js
+++ main.js
@@ -115,6 +115,7 @@ class MihomeVacuum extends utils.Adapter {
     isUnsupportedFeature(key) {
         return this.unsupportedFeatures.indexOf(`|${key}|`) >= 0;
     }
+
     setUnsupportedFeature(key) {
         if (this.unsupportedFeatures.indexOf(`|${key}|`) == -1) {
             this.unsupportedFeatures += `${key}|`;
@@ -198,53 +199,53 @@ class MihomeVacuum extends utils.Adapter {
         }
         return manager;
         /* const deviceList = {
-    //'mijia.vacuum.v2' : MiotVacuum, //  Modell: Xiaomi mijia g1
-    //'dreame.vacuum.mc1808' : MiotVacuum, //  Modell: Xioami mijia 1c
-    'viomi.vacuum.v7': ViomiManager,
-    'viomi.vacuum.v8': ViomiManager,
-    'viomi.vacuum.v19': ViomiManager, //test
-    'viomi.vacuum.v13': ViomiManager, // added for test
-    'roborock.vacuum.s4': VacuumManager, // Roborock S4
-    'roborock.vacuum.s5': VacuumManager,
-    'roborock.vacuum.s5e': VacuumManager, // Roborock S5 Max
-    'roborock.vacuum.s6': VacuumManager,
-    'roborock.vacuum.a08': VacuumManager, // Roborock S6 Pure
-    'roborock.vacuum.m1s': VacuumManager,
-    'rockrobo.vacuum.v1': VacuumManager,
-    'roborock.vacuum.a10': VacuumManager, // Roborock S6 MaxV
-    'roborock.vacuum.a15': VacuumManager, // Roborock S7
-    'roborock.vacuum.a27': VacuumManager, // Roborock S7 MaxV
-    'roborock.vacuum.a38': VacuumManager, // Roborock Q7 Max
-    'roborock.vacuum.a51': VacuumManager, // Roborock S8
-    'roborock.vacuum.a62': VacuumManager, // Roborock S7 Pro Ultra
-    'roborock.vacuum.a70': VacuumManager, // Roborock S8 Ultra Pro
-    'roborock.vacuum.a74': VacuumManager, // Roborock P10
-    // 'roborock.sweeper.e2v3': VacuumManager2,
-    // 'roborock.sweeper.e2v2': VacuumManager2,
-    // 'roborock.vacuum.e2': VacuumManager2,
-    // 'roborock.sweeper.c1v3': VacuumManager2,
-    // 'roborock.sweeper.c1v2': VacuumManager2,
-    // 'roborock.vacuum.c1': VacuumManager2,
-    // 'roborock.vacuum.a01': VacuumManager2,
-    // 'roborock.vacuum.a01v2': VacuumManager2,
-    // 'roborock.vacuum.a01v3': VacuumManager2,
-    // 'roborock.vacuum.a04': VacuumManager2,
-    // 'roborock.vacuum.a04v2': VacuumManager2,
-    // 'roborock.vacuum.a04v3': VacuumManager2
-    'dreame.vacuum.r2205': DreameManager, // Dreame D10 Plus
-    'dreame.vacuum.r2216o': DreameManager, // Dreame L10S Pro
-    'dreame.vacuum.r2228o': DreameManager, // Dreame L10S Ultra
-    'dreame.vacuum.p2008': DreameManager, // Dreame F9
-    'dreame.vacuum.p2009': DreameManager, // Dreame D9
-    'dreame.vacuum.p2027': DreameManager, // Dreame W10
-    'dreame.vacuum.p2028': DreameManager, // Dreame Z10 Pro
-    'dreame.vacuum.p2029': DreameManager, // Dreame L10 Pro
-    'dreame.vacuum.p2036': DreameManager, // Trouver Finder LDS Cleaner
-    'dreame.vacuum.p2041o': DreameManager, // Xiaomi Vacuum Mop 2 Pro+
-    'dreame.vacuum.p2114a': DreameManager, // Xiaomi Robot Vacuum X10 Plus
-    'xiaomi.vacuum.c102gl': DreameManager, // Xiaomi Robot Vacuum X20 Plus
-    'dreame.vacuum.p2148o': DreameManager, // Xiaomi Mijia Ultra Slim
-    'dreame.vacuum.p2156o': DreameManager, // MOVA Z500
+	//'mijia.vacuum.v2' : MiotVacuum, //  Modell: Xiaomi mijia g1
+	//'dreame.vacuum.mc1808' : MiotVacuum, //  Modell: Xioami mijia 1c
+	'viomi.vacuum.v7': ViomiManager,
+	'viomi.vacuum.v8': ViomiManager,
+	'viomi.vacuum.v19': ViomiManager, //test
+	'viomi.vacuum.v13': ViomiManager, // added for test
+	'roborock.vacuum.s4': VacuumManager, // Roborock S4
+	'roborock.vacuum.s5': VacuumManager,
+	'roborock.vacuum.s5e': VacuumManager, // Roborock S5 Max
+	'roborock.vacuum.s6': VacuumManager,
+	'roborock.vacuum.a08': VacuumManager, // Roborock S6 Pure
+	'roborock.vacuum.m1s': VacuumManager,
+	'rockrobo.vacuum.v1': VacuumManager,
+	'roborock.vacuum.a10': VacuumManager, // Roborock S6 MaxV
+	'roborock.vacuum.a15': VacuumManager, // Roborock S7
+	'roborock.vacuum.a27': VacuumManager, // Roborock S7 MaxV
+	'roborock.vacuum.a38': VacuumManager, // Roborock Q7 Max
+	'roborock.vacuum.a51': VacuumManager, // Roborock S8
+	'roborock.vacuum.a62': VacuumManager, // Roborock S7 Pro Ultra
+	'roborock.vacuum.a70': VacuumManager, // Roborock S8 Ultra Pro
+	'roborock.vacuum.a74': VacuumManager, // Roborock P10
+	// 'roborock.sweeper.e2v3': VacuumManager2,
+	// 'roborock.sweeper.e2v2': VacuumManager2,
+	// 'roborock.vacuum.e2': VacuumManager2,
+	// 'roborock.sweeper.c1v3': VacuumManager2,
+	// 'roborock.sweeper.c1v2': VacuumManager2,
+	// 'roborock.vacuum.c1': VacuumManager2,
+	// 'roborock.vacuum.a01': VacuumManager2,
+	// 'roborock.vacuum.a01v2': VacuumManager2,
+	// 'roborock.vacuum.a01v3': VacuumManager2,
+	// 'roborock.vacuum.a04': VacuumManager2,
+	// 'roborock.vacuum.a04v2': VacuumManager2,
+	// 'roborock.vacuum.a04v3': VacuumManager2
+	'dreame.vacuum.r2205': DreameManager, // Dreame D10 Plus
+	'dreame.vacuum.r2216o': DreameManager, // Dreame L10S Pro
+	'dreame.vacuum.r2228o': DreameManager, // Dreame L10S Ultra
+	'dreame.vacuum.p2008': DreameManager, // Dreame F9
+	'dreame.vacuum.p2009': DreameManager, // Dreame D9
+	'dreame.vacuum.p2027': DreameManager, // Dreame W10
+	'dreame.vacuum.p2028': DreameManager, // Dreame Z10 Pro
+	'dreame.vacuum.p2029': DreameManager, // Dreame L10 Pro
+	'dreame.vacuum.p2036': DreameManager, // Trouver Finder LDS Cleaner
+	'dreame.vacuum.p2041o': DreameManager, // Xiaomi Vacuum Mop 2 Pro+
+	'dreame.vacuum.p2114a': DreameManager, // Xiaomi Robot Vacuum X10 Plus
+	'xiaomi.vacuum.c102gl': DreameManager, // Xiaomi Robot Vacuum X20 Plus
+	'dreame.vacuum.p2148o': DreameManager, // Xiaomi Mijia Ultra Slim
+	'dreame.vacuum.p2156o': DreameManager, // MOVA Z500
 };*/
     }
 
@@ -318,7 +319,33 @@ class MihomeVacuum extends utils.Adapter {
     async onReady() {
         // Reset the connection indicator during startup
         this.setConnection(false);
+        await this.ensureAuthStates();
+        XiaomiApi = new XiaomiCloudConnector(this.log, {}, this);
         this.main();
+    }
+
+    async ensureAuthStates() {
+        await this.setObjectNotExistsAsync('auth', {
+            type: 'channel',
+            common: { name: 'Xiaomi Cloud authentication' },
+            native: {},
+        });
+        const states = {
+            status: { name: 'Authentication status', type: 'string', role: 'text', def: 'not_authenticated' },
+            loginUrl: { name: 'Xiaomi QR login URL', type: 'string', role: 'text.url', def: '' },
+            lastError: { name: 'Last authentication error', type: 'string', role: 'text', def: '' },
+            expiresAt: { name: 'QR login expiry timestamp', type: 'number', role: 'value.time', def: 0 },
+        };
+        await Promise.all(
+            Object.entries(states).map(([id, common]) =>
+                this.setObjectNotExistsAsync(`auth.${id}`, {
+                    type: 'state',
+                    common: { ...common, read: true, write: false },
+                    native: {},
+                }),
+            ),
+        );
+        await this.setStateAsync('auth.status', 'not_authenticated', true);
     }
 
     /**
@@ -328,6 +355,7 @@ class MihomeVacuum extends utils.Adapter {
      */
     async onUnload(callback) {
         try {
+            XiaomiApi?.shutdown();
             if (vacuum) {
                 await vacuum.close();
             }
@@ -434,27 +462,29 @@ class MihomeVacuum extends utils.Adapter {
             switch (obj.command) {
                 case 'discovery': {
                     if (!XiaomiApi) {
-                        XiaomiApi = new XiaomiCloudConnector(this.log, obj.message.authObj);
+                        XiaomiApi = new XiaomiCloudConnector(this.log, obj.message.authObj, this);
                     } else {
                         XiaomiApi.init(obj.message.authObj);
                     }
-                    XiaomiApi.login()
-                        .then(result => {
-                            if (result.ok) {
-                                return XiaomiApi.getDevices(obj.message.server).then(data => {
-                                    this.log.debug(`discover__${JSON.stringify(data)}`);
-                                    respond(data);
-                                    return;
-                                });
-                            }
-                            respond(result);
-                        })
-                        .catch(result => {
-                            this.log.info(`discover ${result.err}`);
-                            respond(result);
-                        });
+                    const result = await XiaomiApi.login();
+                    if (result.ok) {
+                        try {
+                            respond(await XiaomiApi.getDevices(obj.message.server));
+                        } catch (error) {
+                            respond({ err: error instanceof Error ? error.message : 'Could not retrieve Xiaomi devices' });
+                        }
+                    } else {
+                        respond(result);
+                    }
                     return;
                 }
+
+                case 'startCloudLogin': {
+                    if (!XiaomiApi) XiaomiApi = new XiaomiCloudConnector(this.log, {}, this);
+                    respond(await XiaomiApi.startQrLogin());
+                    return;
+                }
+
                 // ======================================================================
                 default:
                     //respond(predefinedResponses.ERROR_UNKNOWN_COMMAND);
